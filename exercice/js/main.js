0f37e8f92ecb9458607b86d2141a3a6d5a0d6f63
Damien Roelens <roeldam16@gmail.com>
2018-01-29 11:23:07 +0100

    Minification and optimisation
    Added minified files and changed links, corrected errors in css

--- exercice/js/main.js
+++ exercice/js/main.js
@@ -1,6 +1,8 @@
 window.onload = function(){
   // When the user scrolls the page, execute myFunction
-  window.onscroll = function() {setStickyMenu()};
+  window.onscroll = function() {
+    setStickyMenu()
+  };
 
   // Get the navbar
   var navbar = document.getElementById("stickyMenu");
@@ -11,12 +13,12 @@ window.onload = function(){
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function setStickyMenu() {
     if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
+      navbar.classList.add("sticky");
     } else {
       navbar.classList.remove("sticky");
     }
   }
-}
+};
 
 // Function to add class to the top nav and generate a responsive menu with css
 function responsiveMenu() {
